f2a3291c7d94235d515754e135d907c66d7c092a
TAV <tereshenkov@mail.ru>
2023-07-21 17:50:16 +0300

    Добавлено создание архивов для Linux.

--- setup/windows/build64.bat
+++ setup/windows/build64.bat
@@ -1,3 +1,4 @@
+del /q ..\..\build-LunaDenyCakesGame\*
 dotnet publish ..\..\LunaDenyCakesGame\LunaDenyCakesGame\LunaDenyCakesGame.csproj -r win-x64 -c Release --self-contained -o ..\..\build-LunaDenyCakesGame
 "C:\Program Files (x86)\NSIS\makensis.exe" LunaDenyCakesGame.nsi
 create_zip64.bat
